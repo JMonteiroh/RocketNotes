4eba40bfeae49cb1ebc803f07bc5a713270736c2
João Paulo <joao.paulomonteiro@hotmail.com>
2023-03-02 13:00:50 -0300

    Created page profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,46 @@
+import { FiArrowLeft, FiLock, FiMail, FiUser } from "react-icons/fi";
+
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
+import { Container, Form } from "./styles";
+
+export function Profile() {
+
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft/>
+        </a>
+        <Form>
+          <Input
+            placeholder="Nome"
+            type="text"
+            icon={FiUser}
+          />
+
+          <Input
+            placeholder="E-mail"
+            type="text"
+            icon={FiMail}
+          />
+
+          <Input
+            placeholder="Senha atual"
+            type="password"
+            icon={FiLock}
+          />
+
+          <Input
+            placeholder="Nova senha"
+            type="password"
+            icon={FiLock}
+          />
+
+          <Button title="Salvar"/>
+        </Form>
+      </header>
+    </Container>
+  )
+}
