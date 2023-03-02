7afcdb0e296033c1cb2e139c6156544bb5f9bd1c
João Paulo <joao.paulomonteiro@hotmail.com>
2023-03-02 13:20:45 -0300

    end of page profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiLock, FiMail, FiUser } from "react-icons/fi";
+import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from "react-icons/fi";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
 
@@ -13,7 +13,18 @@ export function Profile() {
         <a href="/">
           <FiArrowLeft/>
         </a>
+      </header>
         <Form>
+          <Avatar>
+            <img src="https://github.com/jmonteiroh.png" alt="Foto do usuário" />
+            <label htmlFor="avatar">
+              <FiCamera />
+              <input 
+                id="avatar"
+                type="file"
+              />
+            </label>
+          </Avatar>
           <Input
             placeholder="Nome"
             type="text"
@@ -40,7 +51,6 @@ export function Profile() {
 
           <Button title="Salvar"/>
         </Form>
-      </header>
     </Container>
   )
 }
